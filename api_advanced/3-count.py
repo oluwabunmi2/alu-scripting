0f52dd0d6a5ef512ff784a5aae3f1308db99f0c1
oluwanumni2 <o.adebanjo@alustudent.com>
2022-10-21 01:10:43 +0000

    :robot: automated commit of one or many files

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -39,3 +39,4 @@ def count_words(subreddit, word_list, found_list=[], after=None):
                                      reverse=True):
                 print('{}: {}'.format(key, value))
     else:
+        return
